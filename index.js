1c1bbe2fc7ec64abb93823d29995618519ef4d54
Peter Igaga <igagapeter477@gmail.com>
2025-06-04 22:54:51 +0100

    deploying data capture to test fingerprint

---
+++ index.js
@@ -0,0 +1,132 @@
+let fingerprintData = null;
+
+const handleFileSelect = async () => {
+  document.getElementById("uploadBtn").addEventListener("click", () => {
+    document.querySelector(".hidden").click();
+
+    document
+      .getElementById("uploadInput")
+      .addEventListener("change", async (fileInput) => {
+        const file = fileInput.target.files[0];
+
+        if (!file) {
+          alert("You ought to upload a file here ");
+          return;
+        }
+
+        const uploadArea = document.getElementById("form-sec2");
+        const imgDiv = document.createElement("div");
+        const uploadedImg = document.createElement("img");
+        uploadedImg.src = URL.createObjectURL(file);
+
+        alert("file added");
+        imgDiv.appendChild(uploadedImg);
+        uploadArea.appendChild(imgDiv);
+        imgDiv.style.maxWidth = "15%";
+        imgDiv.style.maxHeight = "15%";
+        uploadedImg.style.maxWidth = "100%";
+        uploadedImg.style.maxHight = "100%";
+        uploadedImg.onload = URL.revokeObjectURL(uploadedImg.src);
+      });
+  });
+
+  document.getElementById("uploadInput").addEventListener("cancel", () => {
+    alert("You ought to upload an image here ");
+  });
+};
+
+const handleFormUpload = async () => {
+  document
+    .getElementById("myForm")
+    .addEventListener("submit", async (event) => {
+      event.preventDefault();
+
+      if (!window.fingerprintData) {
+        alert("Please scan fingerprint before submit");
+        return;
+      }
+
+      const formInput = event.target;
+      const file = formInput.passport.files[0];
+
+      const fullName = formInput.fullName.value;
+      const staffId = formInput.staffId.value;
+      const dateOfBirth = formInput.dateOfBirth.value;
+      const gender = formInput.gender.value;
+      const ppa = formInput.ppa.value;
+      const numberOfPupils = formInput.pupils.value;
+
+      const formData = new FormData();
+
+      formData.append("fullName", fullName);
+      formData.append("staffId", staffId);
+      formData.append("dateOfBirth", dateOfBirth);
+      formData.append("gender", gender);
+      formData.append("ppa", ppa);
+      formData.append("pupils", numberOfPupils);
+      formData.append("passport", file);
+      formData.append("fingerprintImage", fingerprintData.BitmapData); // base64 string
+      formData.append("fingerprintTemplate", fingerprintData.TemplateBase64); // base64 string
+
+      if (!file || !file.type.startsWith("image/")) {
+        alert("Upload passport photograph");
+        return;
+      }
+
+      //handling file upload here
+      const res = await fetch("http://localhost:3000/api/v1/captureDevice", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (!res.ok) {
+        console.log("form data upload failed");
+        alert("Form Data was not uploaded successfully");
+        return;
+      }
+
+      const response = await res.json();
+
+      alert("User information is created successfully");
+    });
+};
+
+const handleFingerprintScanning = async () => {
+  document.getElementById("scannowID").addEventListener("click", async () => {
+    document.getElementById("display-fingerprint-image").style.display =
+      "block";
+
+    alert("Please ensure Secu Gen scanner is connected");
+
+    try {
+      const response = await fetch("https://localhost:8000/SGIFPCapture", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          Quality: 60,
+          Timeout: 10000,
+          ImageWSQRate: 0.75,
+          TemplateFormat: "ISO",
+        }),
+      });
+
+      const result = await response.json();
+
+      if (result.ErrorCode == 0) {
+        fingerprintData = result;
+        document.getElementById("fingerprint-imageID").src =
+          "./images/scanaccepted.jpg";
+      } else {
+        alert(`${result.errorDescription}`);
+      }
+    } catch (error) {
+      throw new Error("scanning error, try again ", error);
+    }
+  });
+};
+
+handleFormUpload();
+handleFingerprintScanning();
+handleFileSelect();
